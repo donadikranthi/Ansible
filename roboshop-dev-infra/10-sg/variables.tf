4408b535adf759875f0a0f5c99332eea99886bc3
donadikranthi <donadikranthikumar@gmail.com>
2025-11-09 06:53:45 +0530

    main

--- roboshop-dev-infra/10-sg/variables.tf
+++ roboshop-dev-infra/10-sg/variables.tf
@@ -6,9 +6,27 @@ variable "environment"{
 default ="dev"
 }
 
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
 variable "sg_names" {
     default = [
-    "mongodb","redis","mysql","rabbitmq",
-        "bastion"
-        ]
+        # databases
+        "mongodb", "redis", "mysql", "rabbitmq",
+        # backend
+        "catalogue", "user", "cart", "shipping", "payment",
+        # frontend
+        "frontend",
+        # bastion
+        "bastion",
+        # frontend load balancer
+        "frontend_alb",
+        # Backend ALB
+        "backend_alb"
+    ]
 }

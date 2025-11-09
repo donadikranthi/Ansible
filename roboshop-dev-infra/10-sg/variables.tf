29061b1e9fd19939f4bb1f044508f48933e7c7b5
donadikranthi <donadikranthikumar@gmail.com>
2025-11-09 06:59:12 +0530

    main

--- roboshop-dev-infra/10-sg/variables.tf
+++ roboshop-dev-infra/10-sg/variables.tf
@@ -6,13 +6,6 @@ variable "environment"{
 default ="dev"
 }
 
-variable "project_name" {
-    default = "roboshop"
-}
-
-variable "environment" {
-    default = "dev"
-}
 
 variable "sg_names" {
     default = [

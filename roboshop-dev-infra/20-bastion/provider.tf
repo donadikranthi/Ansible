9a8f522cf93a87b0c82d1210a7d18cfbbc64d003
donadikranthi <donadikranthikumar@gmail.com>
2025-11-15 13:56:07 +0530

    main

--- roboshop-dev-infra/20-bastion/provider.tf
+++ roboshop-dev-infra/20-bastion/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket = "remote-state-kranthi-dev"
-    key    = "roboshop-kranthi-bastion"
+    key    = "roboshop-dev-bastion"
     region = "us-east-1"
     use_lockfile = true
     encrypt = true

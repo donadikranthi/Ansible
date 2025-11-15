91f254560b32ccd91667cb32db43397b04d13016
donadikranthi <donadikranthikumar@gmail.com>
2025-11-15 14:24:15 +0530

    main

--- roboshop-dev-infra/20-bastion/main.tf
+++ roboshop-dev-infra/20-bastion/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "bastion" {
     ami = local.ami_id
-    instance_type = var.instance_type
+    instance_type = "t3.micro"
     vpc_security_group_ids = [local.bastion_sg_id]
     
     

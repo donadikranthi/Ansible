8df91f115727dbf63e399b86e995c21fcf29350b
donadikranthi <donadikranthikumar@gmail.com>
2025-11-15 12:54:32 +0530

    main

--- roboshop-dev-infra/10-sg/main.tf
+++ roboshop-dev-infra/10-sg/main.tf
@@ -11,10 +11,20 @@
 
 module "sg" {
   count = length(var.sg_names)
-  source = "git::https://github.com/daws-86s/terraform-aws-sg.git?ref=main"
+  source = "git::https://github.com/donadikranthi/terraform-aws-sg.git?ref=main"
   project_name = var.project_name
   environment = var.environment
   sg_name = var.sg_names[count.index]
   sg_description = "Created for ${var.sg_names[count.index]}"
   vpc_id =  local.vpc_id
 }
+
+# # Frontend accepting traffic from frontend ALB
+# resource "aws_security_group_rule" "frontend_frontend_alb" {
+#   type              = "ingress"
+#   security_group_id = module.sg[9].sg_id # frontend SG ID
+#   source_security_group_id = module.sg[11].sg_id # frontend ALB SG ID
+#   from_port         = 80
+#   protocol          = "tcp"
+#   to_port           = 80
+# }
